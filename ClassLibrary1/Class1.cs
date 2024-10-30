129400944e0d8a867e9b0c7ad29f111503037e1b
исп-34 <-34@LAB21-06>
2024-10-30 11:55:28 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassLibrary1
+
+{
+    public class MatrixHelper
+    {
+       
+        public static int CountDistinctColumns(int[,] matrix)
+        {
+            int m = matrix.GetLength(0); // Число строк
+            int n = matrix.GetLength(1); // Число столбцов
+            int count = 0; // Счетчик столбцов с различными элементами
+
+            for (int j = 0; j < n; j++) // Итерация по столбцам
+            {
+                bool allDistinct = true; 
+
+                for (int i = 0; i < m - 1; i++) 
+                {
+                    for (int k = i + 1; k < m; k++) 
+                    {
+                        if (matrix[i, j] == matrix[k, j]) 
+                        {
+                            allDistinct = false; 
+                            break; 
+                        }
+                    }
+
+                    if (!allDistinct)
+                    {
+                        break; 
+                    }
+                }
+
+                if (allDistinct)
+                {
+                    count++; // Увеличение счетчика
+                }
+            }
+
+            return count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[,] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+
+            int distinctColumnsCount = MatrixHelper.CountDistinctColumns(matrix);
+
+            Console.WriteLine($"Количество столбцов с различными элементами: {distinctColumnsCount}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WpfApp15/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace WpfApp15
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                int m = int.Parse(RowsTextBox.Text);
+                int n = int.Parse(ColumnsTextBox.Text);
+
+                int[,] matrix = new int[m, n];   // Создание матрицы
+
+
+                string[] inputValues = InputTextBox.Text.Split(' ');
+                int count = 0;
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = int.Parse(inputValues[count]);
+                        count++;
+                    }
+                }
+
+                
+                int[] maxElements = FindMaxInColumns(matrix, m, n);
+
+                
+                ResultTextBox.Text = "Максимальные элементы в каждом столбце:\n";  // Вывод результата в текстовое поле
+                for (int i = 0; i < n; i++)
+                {
+                    ResultTextBox.Text += $"Столбец {i + 1}: {maxElements[i]}\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private int[] FindMaxInColumns(int[,] matrix, int m, int n)
+        {
+            int[] maxElements = new int[n];
+
+            for (int j = 0; j < n; j++)
+            {
+                maxElements[j] = int.MinValue; // Инициализация минимальным значением
+
+                for (int i = 0; i < m; i++)
+                {
+                    if (matrix[i, j] > maxElements[j])
+                    {
+                        maxElements[j] = matrix[i, j];
+                    }
+                }
+            }
+
+            return maxElements;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
+       
+
